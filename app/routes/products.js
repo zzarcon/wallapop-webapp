0733891d767aded2b4ad5d1c144c52d55675985e
Enric Lluelles <enric@lluell.es>
2014-10-23 01:12:01 +0200

    Very dirty code, but infinite scrolling working

--- app/routes/products.js
+++ app/routes/products.js
@@ -3,11 +3,21 @@ import Product from '../models/product';
 
 export default Ember.Route.extend({
   model: function() {
+    if (!this.get('controller.length')) {
+      return this.loadRecords();
+    }
+  },
 
+  queryURL: function(start) {
+    return 'https://pro2.wallapop.com/shnm-portlet/api/v1/item.json/search7?start=' + start;
+  },
+
+  loadRecords: function(s) {
+    var start = this.get('controller.length') || 0;
     var route = this;
 
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      Ember.$.get(this.queryURL()).then(function(result) {
+      Ember.$.get(route.queryURL(start)).then(function(result) {
         var parsedItems = result.items.map(function(item) {
           return Product.create(item.item);
         });
@@ -17,7 +27,15 @@ export default Ember.Route.extend({
     });
   },
 
-  queryURL: function() {
-    return 'https://pro2.wallapop.com/shnm-portlet/api/v1/item.json/search7'
+  actions: {
+    loadMore: function() {
+      if (!this.loadingMore) {
+        this.loadingMore = true;
+        this.loadRecords().then(function(records) {
+          this.get('controller').pushObjects(records);
+          this.loadingMore = false;
+        }.bind(this));
+      }
+    }
   }
 });
--- app/views/products.js
+++ app/views/products.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  elementId: "products"
+  elementId: "products",
+
+  bindToScroll: function() {
+    $(window).on('scroll', this.scroll.bind(this));
+  }.on('didInsertElement'),
+
+  unbindFromScroll: function() {
+    $(window).off('scroll', this.scroll.bind(this));
+  }.on('willDestroy'),
+
+  scroll: function() {
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      this.get('controller').send('loadMore');
+    }
+  }
 });
