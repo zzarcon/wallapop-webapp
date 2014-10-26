f875aef402f1f550a6288e5c75263ef0dcf1396a
Enric Lluelles <enric@lluell.es>
2014-10-26 16:20:09 +0100

    Added filtering the search query

--- app/services/query_builder.js
+++ app/services/query_builder.js
@@ -1,6 +1,21 @@
 export default Ember.Object.extend({
-  productsURL: function(startItem) {
-    return 'https://pro2.wallapop.com/shnm-portlet/api/v1/item.json/search7?start=' + startItem;
+
+  baseSearchURL: 'https://pro2.wallapop.com/shnm-portlet/api/v1/item.json/search7',
+
+  productsURL: function(startItem, filters) {
+    var params = [];
+
+    if (filters) {
+      filters.categories && params.push('categoryIds=' + filters.categories.join(","));
+
+      filters.price && params.push('salePriceSegments=' + [filters.price.min, filters.price.max].join("_"));
+
+      filters.distance && params.push('distanceSegments=' + [filters.distance.min, filters.distance.max].join("_"));
+    }
+
+    startItem && params.push('start =' + startItem);
+
+    return [this.baseSearchURL, params.join("&")].join("?");
   },
 
   bootstrapURL: function() {
