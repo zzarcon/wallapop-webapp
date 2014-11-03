78b840957a8dc752660518e7c9ec11f1b9adc634
Enric Lluelles <enric@lluell.es>
2014-11-03 20:35:38 +0100

    Moar

--- app/models/category.js
+++ app/models/category.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  selected: DS.attr('boolean', {defaultValue: false})
 });
