093d4258fb0c6f99f96f44aafa9023569eeee68b
Enric Lluelles <enric@lluell.es>
2014-11-06 11:51:14 +0100

    geolocation with promiseproxy

--- app/controllers/products/index.js
+++ app/controllers/products/index.js
@@ -7,22 +7,18 @@ export default Ember.ArrayController.extend({
 
   _latitude: Ember.computed.alias('geolocation.latitude'),
   _longitude: Ember.computed.alias('geolocation.longitude'),
-  _geolocation: function() {
+
+  geolocation: function() {
     var controller = this;
+
     var promise = new Ember.RSVP.Promise(function(resolve) {
       navigator.geolocation.getCurrentPosition(function(location) {
-        debugger;
         resolve(location.coords);
       });
     });
 
-    return DS.PromiseObject.create({promise: promise});
+    return DS.PromiseObject.create({
+      promise: promise
+    });
   }.property(),
-
-  setCoordinates: function() {
-    navigator.geolocation.getCurrentPosition(function(location) {
-      this.set('latitude', location.coords.latitude);
-      this.set('longitude', location.coords.longitude);
-    }.bind(this));
-  }.on('init')
 });
