93da08d2652d35aa02f04ef463eaba0aed843028
Joan Romano <joanromano@gmail.com>
2014-11-16 00:08:08 +0100

    Update style and mark up

--- app/models/user.js
+++ app/models/user.js
@@ -4,10 +4,24 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   microName: DS.attr('string'),
   location: DS.attr(),
-  statsUser: DS.attr(),
+  statsUser: DS.attr(), // favoritesCount, receivedReviewsCount, selledCount, sellingCount, sendReviewsCount
   userId: DS.attr(),
   userVerification: DS.attr('object'),
   image: DS.attr(),
   smallImage: Ember.computed.alias('image.smallURL'),
-  mediumImage: Ember.computed.alias('image.mediumURL')
+  mediumImage: Ember.computed.alias('image.mediumURL'),
+  defaultStatValue: 0,
+  sellingCount : function() {
+    return this.get('statsUser.sellingCount') || 0;
+  }.property('statsUser.sellingCount'),
+  selledCount : function() {
+    return this.get('statsUser.selledCount') || 0;
+  }.property('statsUser.selledCount'),
+  receivedReviewsCount : function() {
+    return this.get('statsUser.receivedReviewsCount') || 0;
+  }.property('statsUser.receivedReviewsCount'),
+  isEmailVerified: Ember.computed.equal('userVerification.emailVerifiedStatus', 30),
+  isCellPhoneVerified: Ember.computed.equal('userVerification.mobileVerifiedStatus', 30),
+  isFacebookVerified: Ember.computed.equal('userVerification.facebookVerifiedStatus', 30),
+  isGooglePlusVerified: Ember.computed.equal('userVerification.googlePlusVerifiedStatus', 30)
 });
