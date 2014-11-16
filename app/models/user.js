0b2fb2b4ff9cd7866bd9514f255c3e8a37cfd24e
Hector Zarco <hezarco@gmail.com>
2014-11-16 15:12:54 +0100

    Showing default user profile image

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
+var verificationValue = 30;
+var equal = Ember.computed.equal;
+
 export default DS.Model.extend({
   microName: DS.attr('string'),
   location: DS.attr(),
@@ -8,20 +11,26 @@ export default DS.Model.extend({
   userId: DS.attr(),
   userVerification: DS.attr('object'),
   image: DS.attr(),
-  smallImage: Ember.computed.alias('image.smallURL'),
+
   mediumImage: Ember.computed.alias('image.mediumURL'),
-  defaultStatValue: 0,
+  isEmailVerified: equal('userVerification.emailVerifiedStatus', verificationValue),
+  isCellPhoneVerified: equal('userVerification.mobileVerifiedStatus', verificationValue),
+  isFacebookVerified: equal('userVerification.facebookVerifiedStatus', verificationValue),
+  isGooglePlusVerified: equal('userVerification.googlePlusVerifiedStatus', verificationValue),
+
+  smallImage: function() {
+    return this.get('image.smallURL') || "assets/images/default-profile-image.png";
+  }.property('image.smallURL'),
+
   sellingCount : function() {
     return this.get('statsUser.sellingCount') || 0;
   }.property('statsUser.sellingCount'),
+
   selledCount : function() {
     return this.get('statsUser.selledCount') || 0;
   }.property('statsUser.selledCount'),
+
   receivedReviewsCount : function() {
     return this.get('statsUser.receivedReviewsCount') || 0;
-  }.property('statsUser.receivedReviewsCount'),
-  isEmailVerified: Ember.computed.equal('userVerification.emailVerifiedStatus', 30),
-  isCellPhoneVerified: Ember.computed.equal('userVerification.mobileVerifiedStatus', 30),
-  isFacebookVerified: Ember.computed.equal('userVerification.facebookVerifiedStatus', 30),
-  isGooglePlusVerified: Ember.computed.equal('userVerification.googlePlusVerifiedStatus', 30)
+  }.property('statsUser.receivedReviewsCount')
 });
