e3da2e8d9a82558c84ed581491d24fb345889f02
Miguel Camba <miguel.camba@gmail.com>
2014-11-23 10:41:26 +0000

    Fixed bower imports. Use ember-moment. Update CSP

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,10 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    contentSecurityPolicy: {
+      'img-src': "'self' *.cloudfront.net *.tiles.mapbox.com",
+      'connect-src': "'self' https://*.wallapop.com"
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
