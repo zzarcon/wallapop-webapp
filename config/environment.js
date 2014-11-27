32482ddfe26887a7643748978a3c54f769cdcfa8
Miguel Camba <miguel.camba@gmail.com>
2014-11-27 00:08:41 +0000

    Don't use bind-attr ever again

--- config/environment.js
+++ config/environment.js
@@ -14,7 +14,8 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-        'ember-htmlbars': true
+        'ember-htmlbars': true,
+        'ember-htmlbars-inline-if-helper': true
       }
     },
 
