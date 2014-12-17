3398fb4a518c7d94a67560976c803660071a1cc2
Miguel Camba <miguel.camba@gmail.com>
2014-12-17 13:40:19 +0000

    Problems updating to canary:
    * Location "auto" does not seems to work.
    * HTMLBars interpolations work bardly in the background images.

--- config/environment.js
+++ config/environment.js
@@ -33,6 +33,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.locationType = 'history';
   }
 
   if (environment === 'test') {
