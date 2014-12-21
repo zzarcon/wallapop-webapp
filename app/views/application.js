e03ec8273fe6e95e785d125aa851b81e5ab27fa0
Miguel Camba <miguel.camba@gmail.com>
2014-12-21 15:40:02 +0000

    It works (more or less) but performs very bad.

--- app/views/application.js
+++ app/views/application.js
@@ -9,20 +9,44 @@ export default Ember.View.extend({
     Ember.$('#init-loader').remove();
   }.on('didInsertElement'),
 
-  touchStart: function(){
-    console.log('touchStart');
+  measureViewport: function () {
+    this.width = this.$().width();
+    this.applicationMenu = this.$('#application-menu')[0];
+    this.menuContent     = this.$('#application-menu .menu-content')[0];
+  }.on('didInsertElement'),
+
+  touchStart: function(evt){
+    var x = evt.originalEvent.touches[0].pageX;
+    if (x < 5) {
+      console.log('movement started near the left side of the screen');
+    } else if (this.width - x < 5) {
+      console.log('movement started near the right side of the screen');
+    } else {
+      return;
+    }
+    this.startX = x;
   },
 
   touchMove: function (evt) {
-    console.log('touchMove', evt);
+    var x = evt.originalEvent.touches[0].pageX;
+    if (this.startX < 10) {
+      this._displaceLeftMenuTo(x);
+    } else if (this.width - this.startX < 10){
+      this._displaceRightMenuTo(x);
+    }
   },
 
   touchEnd: function () {
-    console.log('touch end!');
+    if (this.translation < 0 ){
+      this._collapseLeftMenu();
+    }
+    this.startX = null;
   },
 
   touchCancel: function () {
     console.log('touchCancel');
+    this._collapseLeftMenu();
+    this.startX = null;
   },
 
   actions: {
@@ -38,5 +62,23 @@ export default Ember.View.extend({
       this.set('showFilters', false);
       this.get('controller').send('filterProducts');
     }
+  },
+
+  // private
+
+  _displaceLeftMenuTo: function (x) {
+    var opacity = x / 320;
+    this.translation = Math.min(- 320 + x, 0);
+
+    this.applicationMenu.style.opacity = opacity;
+    this.menuContent.style.transform = 'translateX(' + this.translation + 'px)';
+  },
+
+  _displaceRightMenuTo: function (x) {
+    console.log('displace right menu to: ', x);
+  },
+
+  _collapseLeftMenu: function(){
+    this._displaceLeftMenuTo(0); // TODO: Animate collapse
   }
 });
