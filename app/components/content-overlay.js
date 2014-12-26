0cc4fe8ff911bd5b38cbd97c83fd80970b5851aa
Miguel Camba <miguel.camba@gmail.com>
2014-12-26 00:13:06 +0000

    Apply style in a more declarative way.

--- app/components/content-overlay.js
+++ app/components/content-overlay.js
@@ -1,13 +1,10 @@
 import Ember from 'ember';
 
-function updadateStyle(){
-  var visibility = this.get('visibility');
-  this.element.style.opacity = visibility;
-  this.element.style['z-index'] = visibility > 0 ? 99 : -1;
-}
-
 export default Ember.Component.extend({
-  updateStyle: function() {
-    Ember.run.scheduleOnce('afterRender', this, updadateStyle);
-  }.observes('visibility')
+  attributeBindings: ['style'],
+  style: function() {
+    var opacity = this.get('opacity');
+    var zIndex = opacity > 0 ? 99 : -1;
+    return 'opacity: ' + opacity + '; z-index: ' + zIndex + ';';
+  }.property('opacity')
 });
--- app/components/slider-menu.js
+++ app/components/slider-menu.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Gesture from "wallapop-webapp/utils/gesture";
 
 export default Ember.Component.extend({
+  attributeBindings: ['style'],
   position: 'left',
   progress: 0,
   defaultSpeed: null, // Needs to be passed in
@@ -9,7 +10,6 @@ export default Ember.Component.extend({
   // Initializers
   setTouchEventListeners: function() {
     this.width = this.element.offsetWidth;
-    this.progressObserver();
     var applicationNode = document.querySelector('#application');
     var self = this;
 
@@ -51,6 +51,11 @@ export default Ember.Component.extend({
     return this.get('progress') <= 0;
   }.property('progress'),
 
+  style: function(){
+    var translate = (this.get('progress') - 1) * this.width;
+    return 'transform: translateX(' + translate + 'px); -webkit-transform: translateX(' + translate + 'px)';
+  }.property('progress'),
+
   // Private methods
   mustFollow: function(gesture){
     return this.get('expanded') || this.position === 'left' && gesture.initPageX() < 20;
@@ -81,11 +86,5 @@ export default Ember.Component.extend({
     if (newProgress > 0 && newProgress < 1) {
       requestAnimationFrame(this.completeExpansion.bind(this));
     }
-  },
-
-  progressObserver: function(){
-    var translate = (this.get('progress') - 1) * this.width;
-    this.element.style.transform  = 'translateX(' + translate + 'px)';
-    this.element.style['-webkit-transform'] = 'translateX(' + translate + 'px)';
-  }.observes('progress')
+  }
 });
